ffc47e3b1bb5c9f04877673ca0f72b81dd23fb8c
Seth Swanson <sethswanson@fastmail.com>
2023-03-08 10:27:50 -0700

    Adde var for infracost api key

--- variables.tf
+++ variables.tf
@@ -772,3 +772,9 @@ variable "alb_enable_cross_zone_load_balancing" {
   type        = bool
   default     = null
 }
+
+variable "infracost_api_key" {
+  description = "API Key Used for authenticating to Infracost for cost projections"
+  type        = string
+  default     = "false"
+}
